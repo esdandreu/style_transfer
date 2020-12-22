0e6f0c878d726e1955a21c5db34df25782515e2f
--get <tech.marketing@prontopro.it>
2020-12-22 13:20:23 +0100

    blur images

--- test_experiment.py
+++ test_experiment.py
@@ -7,8 +7,22 @@ from pathlib import Path
 
 logger = config_logger(Path('output'))
 
-folder = Path('results', '20201221_105909 - Copy')
-r = Experiment(folder)
+import cv2
+folder = Path('datasets')
+folder.mkdir(exist_ok=True)
+
+for img_name in [r'content\turtle', r'style\kanagawa']:
+    filename = Path(folder, 'style_transfer', f'{img_name}.jpg')
+    img = cv2.imread(str(filename))
+    for k in [5,11,19,43,101,151,211,401]:
+        logger.info(k)
+        blur = cv2.GaussianBlur(img,(k,k),0)
+        filename = Path(folder, 'blur', f'{img_name}_blur{k}.jpg')
+        filename.parent.mkdir(exist_ok=True, parents=True)
+        assert cv2.imwrite(str(filename),blur)
+
+# folder = Path('results', 'parameters_experiments')
+# r = Experiment(folder)
 
 # logger.info(f'{r.options(STYLE_WEIGHT, "turtle", "kanagawa") = }')
 # logger.info(f'{r.options(CONTENT_LAYERS, "turtle", "kanagawa") = }')
